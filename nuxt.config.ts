95e5dd3cf91df852da160b715f3bcc8631d2b9eb
jberg <jbergay@jgingenieros.es>
2023-01-24 18:54:09 +0100

    chore: add ts and base url to nuxt config

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,4 +1,10 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
-
-})
+  typescript: {
+    shim: false,
+    strict: true,
+  },
+  appConfig: {
+    SW_API: 'https://swapi.dev/api'
+  },
+});
